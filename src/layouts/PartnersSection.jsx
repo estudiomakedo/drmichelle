ec51ef23f14b6266fcea2aa218391df401e65bd1
Franklin Silva <fsilva@iMac-de-Brisa.local>
2019-12-19 09:04:47 -0300

    teste3

--- src/layouts/PartnersSection.jsx
+++ src/layouts/PartnersSection.jsx
@@ -47,13 +47,11 @@ const CardRow = styled.div`
   display: flex;
   margin: 0 auto;
   flex-direction: row;
+  align-items: center;
   flex-wrap: wrap;
-  
+  justify-content: space-evenly;
   @media screen and (min-width:  ${props => props.theme.breakpoints.s}){
     flex-direction: row;
-    justify-content: space-evenly;
-    align-items: center;
-
   }
 
   @media screen and (min-width:  ${props => props.theme.breakpoints.l}){
@@ -67,9 +65,7 @@ const Card = styled.div`
   border-radius: 20px;
   padding: 15px;
   margin: 15px 15px 20px;
-  display: flex;
   width: 150px;
-  flex-wrap: wrap;
   @media screen and (min-width:  ${props => props.theme.breakpoints.s}){
     margin-bottom: 0;
     max-width: 300px;
